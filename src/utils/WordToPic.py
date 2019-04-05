ca0d5c00972a8a26392453d5888259eecb0b71d2
quyuanhang <30405886+quyuanhang@users.noreply.github.com>
2019-04-05 10:50:57 +0800

    update

--- src/utils/WordToPic.py
+++ src/utils/WordToPic.py
@@ -1,21 +1,35 @@
 #encoding: utf-8
 import os
 import pygame
+from PIL import Image
+import json
+
+if __name__ == "__main__":
+    data = "multi_data7_tech"
+    data_path = 
+    with open()
 
 chinese_dir = 'chinese'
 if not os.path.exists(chinese_dir):
     os.mkdir(chinese_dir)
 
 pygame.init()
-# for codepoint in range(int(start), int(end)):
-#     word = unichr(codepoint)
-#     font = pygame.font.Font("msyh.ttc", 64)
-#     # 当前目录下要有微软雅黑的字体文件msyh.ttc,或者去c:\Windows\Fonts目录下找
-#     # 64是生成汉字的字体大小
-#     rtext = font.render(word, True, (0, 0, 0), (255, 255, 255))
-#     pygame.image.save(rtext, os.path.join(chinese_dir, word + ".png"))
+
+
 
 word = "腾讯"
-font = pygame.font.Font("msyh.ttc", 64)
-rtext = font.render(word, True, (0, 0, 0), (255, 255, 255))
+font = pygame.font.Font("data/msyh.ttc", 64)
+rtext = font.render(word, True, (0, 0, 0))
 pygame.image.save(rtext, os.path.join(chinese_dir, word + ".png"))
+
+
+arr = ['p1.png', 'p2.png', 'p3.png', 'p4.png']
+toImage = Image.new('RGBA',(400,400))
+for i in range(4):
+    fromImge = Image.open(arr[i])
+    # loc = ((i % 2) * 200, (int(i/2) * 200))
+    loc = ((int(i/2) * 200), (i % 2) * 200)
+    print(loc)
+    toImage.paste(fromImge, loc)
+
+toImage.save('merged.png')
