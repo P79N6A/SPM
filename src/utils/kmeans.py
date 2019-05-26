12008e7e6b0279d71beb9e6710c3b70f9175e577
quyuanhang <30405886+quyuanhang@users.noreply.github.com>
2019-05-26 14:38:03 +0800

    update: 新建kmeans

---
+++ src/utils/kmeans.py
@@ -0,0 +1,10 @@
+from sklearn import cluster
+import pandas as pd
+
+if __name__ == "__main__":
+    data = "multi_data7_300k_pre"
+    fp_pre = "./data/{0}/{0}.w2v".format(data)
+    data = pd.read_csv(fp_pre, sep=" ", header=None).values
+    
+    model = cluster.KMeans(n_clusters=3)
+    cls = model.fit()
