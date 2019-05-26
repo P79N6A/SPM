bc93cf2265b1d59e647cf347b96cde6b312d79aa
yuanhangqu <yuanhangqu@tencent.com>
2019-05-26 17:02:09 +0800

    update：kmeans

--- src/utils/kmeans.py
+++ src/utils/kmeans.py
@@ -1,11 +1,23 @@
 from sklearn import cluster
-import pandas as pd
+from tqdm import tqdm
 
 if __name__ == "__main__":
     data = "multi_data7_300k_pre"
     fp_pre = "./data/{0}/{0}.w2v".format(data)
-    data = pd.read_csv(fp_pre, sep=" ", header=None).values
-    
-    model = cluster.KMeans(n_clusters=3)
-    y = model.fit(data)
-    print(y)
+    data = []
+    with open(fp_pre) as f:
+        for line in tqdm(f):
+            line = line.strip().split()
+            if len(line) != 200:
+                line = line[1:]
+            line = [float(x) for x in line]
+            data.append(line)
+
+    result = []
+    for n in range(1, 10, 2):
+        model = cluster.KMeans(n_clusters=3)
+        model.fit(data)
+        y = model.predict(data)
+        result.append(y)
+
+    with open("./data/multi_data7_300k_pre.")
